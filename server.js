4eaa417e12bffcf82bf278f4d86202a3438d9681
munna1234mm <munna1234mm@gmail.com>
2026-04-07 20:45:14 -0600

    Fixed Render deployment error by using dynamic PORT environment variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: f }) => f(...args));
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // ================================================
 // CONFIG
